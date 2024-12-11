dedc0aed3dfcc1b6f99532500cfd848b6ab784c9
Orie Steele <orie@transmute.industries>
2024-12-11 12:16:53 -0600

    ignore

--- .includes.mk
+++ .includes.mk
@@ -1,6 +1,6 @@
-draft-ietf-cose-dilithium-tmp50465.xml: ./examples/jose/examples/ML_DSA_44.jose.json
-draft-ietf-cose-dilithium-tmp50465.xml: ./examples/jose/examples/ML_DSA_65.jose.json
-draft-ietf-cose-dilithium-tmp50465.xml: ./examples/jose/examples/ML_DSA_87.jose.json
-draft-ietf-cose-dilithium-tmp50465.xml: ./examples/cose/examples/ML_DSA_44.cose.json
-draft-ietf-cose-dilithium-tmp50465.xml: ./examples/cose/examples/ML_DSA_65.cose.json
-draft-ietf-cose-dilithium-tmp50465.xml: ./examples/cose/examples/ML_DSA_87.cose.json
+draft-ietf-cose-dilithium-tmp60035.xml: ./examples/jose/examples/ML_DSA_44.jose.json
+draft-ietf-cose-dilithium-tmp60035.xml: ./examples/jose/examples/ML_DSA_65.jose.json
+draft-ietf-cose-dilithium-tmp60035.xml: ./examples/jose/examples/ML_DSA_87.jose.json
+draft-ietf-cose-dilithium-tmp60035.xml: ./examples/cose/examples/ML_DSA_44.cose.json
+draft-ietf-cose-dilithium-tmp60035.xml: ./examples/cose/examples/ML_DSA_65.cose.json
+draft-ietf-cose-dilithium-tmp60035.xml: ./examples/cose/examples/ML_DSA_87.cose.json
