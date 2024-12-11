c1cb473b0e7acee84efd7f600e69ed942f96e6e3
Orie Steele <orie@transmute.industries>
2024-12-11 12:42:45 -0600

    Delete .includes.mk

--- .includes.mk
+++
@@ -1,6 +0,0 @@
-draft-ietf-cose-dilithium-tmp66678.xml: ./examples/jose/examples/ML_DSA_44.jose.json
-draft-ietf-cose-dilithium-tmp66678.xml: ./examples/jose/examples/ML_DSA_65.jose.json
-draft-ietf-cose-dilithium-tmp66678.xml: ./examples/jose/examples/ML_DSA_87.jose.json
-draft-ietf-cose-dilithium-tmp66678.xml: ./examples/cose/examples/ML_DSA_44.cose.json
-draft-ietf-cose-dilithium-tmp66678.xml: ./examples/cose/examples/ML_DSA_65.cose.json
-draft-ietf-cose-dilithium-tmp66678.xml: ./examples/cose/examples/ML_DSA_87.cose.json
